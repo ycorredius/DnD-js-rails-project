eebceaae2ac0386569a30a8fda619faa9a5e956a
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-09 19:04:45 -0400

    removed unecessary files

--- dnd-api/db/migrate/20200409172636_character_proficencies.rb
+++
@@ -1,4 +0,0 @@
-class CharacterProficencies < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- dnd-api/db/migrate/20200409172710_character_equipment.rb
+++
@@ -1,4 +0,0 @@
-class CharacterEquipment < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
