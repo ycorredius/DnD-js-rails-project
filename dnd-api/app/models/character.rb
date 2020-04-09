b0eead4564c7e81ca0352f9eab4e3e5ebf8066e5
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-09 19:38:24 -0400

    add race association

--- dnd-api/app/models/character.rb
+++ dnd-api/app/models/character.rb
@@ -1,5 +1,6 @@
 class Character < ApplicationRecord
     belongs_to :characterclass
+    belongs_to :race    
     has_many :proficiencies, through: :characterclass
     has_many :equipments, through: :characterclass
 
