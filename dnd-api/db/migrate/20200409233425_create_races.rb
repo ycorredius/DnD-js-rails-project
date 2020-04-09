a5026d4f398a355cdb7779f55427d6df2f9bdbe0
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-09 19:37:45 -0400

    add race model

---
+++ dnd-api/db/migrate/20200409233425_create_races.rb
@@ -0,0 +1,14 @@
+class CreateRaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :races do |t|
+      t.string :name
+      t.string :speed
+      t.string :age
+      t.string :alignment
+      t.string :size_description
+      t.string :language_desc
+
+      t.timestamps
+    end
+  end
+end
