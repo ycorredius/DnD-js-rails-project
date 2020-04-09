8068d97649b44d4f2e44980f2dd890495c4772db
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-09 19:07:41 -0400

    add character class reference

--- dnd-api/db/migrate/20200409171349_create_characters.rb
+++ dnd-api/db/migrate/20200409171349_create_characters.rb
@@ -2,8 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :phrase
-      t.string :img_url
+      t.references :characterclass, null: false, foreign_key: true
       
       t.timestamps
     end
