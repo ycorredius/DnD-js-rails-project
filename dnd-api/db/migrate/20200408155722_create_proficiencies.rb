240423a756b015d42d0c58a61e8e1100a580eac3
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-09 19:08:13 -0400

    change cloumn type and removed url

--- dnd-api/db/migrate/20200408155722_create_proficiencies.rb
+++ dnd-api/db/migrate/20200408155722_create_proficiencies.rb
@@ -1,9 +1,8 @@
 class CreateProficiencies < ActiveRecord::Migration[6.0]
   def change
     create_table :proficiencies do |t|
-      t.string :name
       t.string :index
-      t.string :url
+      t.string :category
       
       t.timestamps
     end
