a2c7457005eb60f59e31af54b1ba57fb14e28b74
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-10 10:37:25 -0400

    fixing model associations

--- dnd-api/app/models/characterclass.rb
+++ dnd-api/app/models/characterclass.rb
@@ -1,4 +1,5 @@
 class Characterclass < ApplicationRecord
+    has_many :characters
     has_many :characterclass_proficiencies
     has_many :proficiencies , through: :characterclass_proficiencies
 
--- dnd-api/app/models/proficiency.rb
+++ dnd-api/app/models/proficiency.rb
@@ -1,6 +1,7 @@
 class Proficiency < ApplicationRecord
     has_many :characterclass_proficiencies
-    has_many :characterclasses, through: :characterclass_proficencies
+    has_many :characterclasses, through: :characterclass_proficiencies
     
     validates :index, uniqueness: true
+
 end
