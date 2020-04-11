e2a08a1bdf8c8c132273c1e9c84712120f0d29d2
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-10 23:50:30 -0400

    built for front end

--- dnd-api/app/controllers/characters_controller.rb
+++ dnd-api/app/controllers/characters_controller.rb
@@ -1,4 +1,10 @@
 class CharactersController < ApplicationController
+    before_action :set_characterclass, only: [:show]
+
     def index
-        
+        @classes = Characterclass.all
+        render json: @classes 
+    end
+
+    
 end
