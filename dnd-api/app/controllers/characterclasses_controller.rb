875ed116cdb4ec8ab4ec61f1bff795474ba34627
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-10 23:46:40 -0400

    building out characterclass controller

--- dnd-api/app/controllers/characterclasses_controller.rb
+++ dnd-api/app/controllers/characterclasses_controller.rb
@@ -1,6 +1,19 @@
 class CharacterclassesController < ApplicationController
+    before_action :set_characterclass, only: [:show]
     def index
-        classes = Characterclass.all
-        render json: classes
+        @classes = Characterclass.all
+        render json: @classes
+    end
+    
+    def show
+        options = {}
+        options[:include] = [:proficiencies,:'proficiencies.id', :'proficiencies.name', :'proficiencies.category']
+        render json: CharacterclassSerializer.new(@characterclass,options).serialized_json
+    end
+
+    private
+
+    def set_characterclass
+       @characterclass = Characterclass.find(params[:id]) 
     end
 end
