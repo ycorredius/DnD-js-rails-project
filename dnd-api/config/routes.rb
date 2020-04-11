99721fce0ddf35ffe3041a87b01467571893d3ab
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-10 23:50:49 -0400

    updates

--- dnd-api/config/routes.rb
+++ dnd-api/config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'welcome#index'
+  get '/route', to: 'controller#method'
+
 end
