b2e66420cb79382d5c648ffcf630bb6ddfad0130
Corredius Yarbrough <ycorredius@gmail.com>
2020-04-15 01:31:21 -0400

    Characterclass show works

--- dnd-api/app/serializers/race_serializer.rb
+++ dnd-api/app/serializers/race_serializer.rb
@@ -1,4 +1,4 @@
 class RaceSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :speed, :age, :alignment, :size_description, :language_desc
+  attributes :name, :speed, :age, :alignment, :size_description, :language_desc,:img_url
 end
